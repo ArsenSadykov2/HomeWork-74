1de4caf7b650f3db9317debc9bafa283c938b9db
Sadykov Arsen <LoCo10291998@gmail.com>
2025-03-18 18:43:13 +0600

    Made creating new files for every new message with date time on name of files.

--- fileDb.ts
+++ fileDb.ts
@@ -1,39 +1,52 @@
 import {promises as fs} from 'fs';
 import {Message, MessageWithoutId} from "./types";
-import {existsSync} from "node:fs";
+import path from "node:path";
 
-const fileName = './db.json';
-let data: Message[] = [];
+const messagesDirectory = './messages';
+const addNewDirMessage = async () => {
+    try {
+        await fs.access(messagesDirectory);
+    } catch {
+        await fs.mkdir(messagesDirectory);
+    }
+};
 
 const fileDb = {
     async init() {
-        try{
-            if(!existsSync(fileName)){
-               await fs.writeFile(fileName, JSON.stringify([]));
-            } else {
-                const fileContent = await fs.readFile(fileName);
-                data = JSON.parse(fileContent.toString()) as Message[];
-            }
-        } catch (e){
-            data = [];
-            console.log(e);
+        await addNewDirMessage();
+    },
+    async getMessageById(dateTime: string): Promise<Message | undefined> {
+        try {
+            const filePath = path.join(messagesDirectory, `${dateTime}.txt`);
+            const fileContent = await fs.readFile(filePath, 'utf-8');
+            return JSON.parse(fileContent);
+        } catch (e) {
+            return undefined;
         }
     },
-    async getMessageById(dateTime: string) {
-        return data.find(m => m.dateTime === dateTime);
-    },
     async getAllMessages() {
-        return data;
+        try {
+            const files = await fs.readdir(messagesDirectory);
+            const messages: Message[] = [];
+
+            for (const file of files) {
+                const filePath = path.join(messagesDirectory, file);
+                const fileContent = await fs.readFile(filePath, 'utf-8');
+                messages.push(JSON.parse(fileContent));
+            }
+
+            return messages;
+        } catch (e) {
+            return [];
+        }
     },
     async addNewMessage(message:MessageWithoutId){
         const dateTime = new Date().toISOString();
-        const newMessage = {dateTime, ...message};
-        data.push(newMessage);
-        await this.save();
+        const newMessage: Message = { dateTime, ...message };
+        const file = path.join(messagesDirectory, `${dateTime}.txt`);
+
+        await fs.writeFile(file, JSON.stringify(newMessage));
         return newMessage;
-    },
-    async save () {
-        return fs.writeFile(fileName, JSON.stringify(data));
     },
 };
 
